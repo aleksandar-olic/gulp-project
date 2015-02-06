0c3ad843aad4466e7ba3250e70a4de7537544982
Aleksandar Olic <aleksandar.olic.89@gmail.com>
2015-02-06 21:36:17 +0100

    Full gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    coffee = require('gulp-coffee'),
+    connect = require('gulp-connect'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+var coffeeSources = ['scripts/hello.coffee'],
+    jsSources = ['scripts/*.js'],
+    sassSources = ['styles/*.scss'],
+    htmlSources = ['**/*.html'],
+    outputDir = 'assets';
+
+
+gulp.task('log', function() {
+  gutil.log('== My First Task ==')
+});
+
+gulp.task('copy', function() {
+  gulp.src('index.html')
+  .pipe(gulp.dest(outputDir))
+});
+
+gulp.task('sass', function() {
+  gulp.src(sassSources)
+  .pipe(sass({style: 'expanded'}))
+    .on('error', gutil.log)
+  .pipe(gulp.dest('assets'))
+  .pipe(connect.reload())
+});
+
+gulp.task('coffee', function() {
+  gulp.src(coffeeSources)
+  .pipe(coffee({bare: true})
+    .on('error', gutil.log))
+  .pipe(gulp.dest('scripts'))
+});
+
+gulp.task('js', function() {
+  gulp.src(jsSources)
+  .pipe(uglify())
+  .pipe(concat('script.js'))
+  .pipe(gulp.dest(outputDir))
+  .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+  gulp.watch(coffeeSources, ['coffee']);
+  gulp.watch(jsSources, ['js']);
+  gulp.watch(sassSources, ['sass']);
+  gulp.watch(htmlSources, ['html']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    livereload: true
+  })
+});
+
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+  .pipe(connect.reload())
+});
+
+gulp.task('default', ['html', 'coffee', 'js', 'sass', 'connect', 'watch']);
